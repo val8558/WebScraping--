72e26c2e557acae6de79f380666cd0e8e74580c9
val8558 <valmmir@hotmail.com>
2023-04-26 23:03:25 -0300

    settings for search on website for a product

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const puppeteer = require('puppeteer');
+const url = 'https://www.mercadolivre.com.br/';
+const ur2 = 'https://www.amazon.com.br/';
+const searchFor = 'repetidor wi-fi';
+let counter = 1;
+const list = [];
 
-(async () => { 
-   const browser = await puppeteer.launch();
-   const page = await browser.newPage();
-   
-   await page.goto('https://www.mercadolivre.com.br/nintendo-switch-32gb-mario-kart-8-deluxe-cor-vermelho-neon-azul-neon-e-preto/p/MLB13576607?pdp_filters=deal:MLB779362-1#searchVariation=MLB13576607&position=2&search_layout=grid&type=product&tracking_id=57e7ff82-7cb9-4ac1-880f-278bdd486e1f&c_id=/home/promotions-recommendations/element&c_element_order=3&c_uid=82cfa2a0-70bd-458e-a86f-09c326612e42');
-   await page.screenshot({path: 'index.png'});
-   
-   await browser.close();
-})();
+    (async () => {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+
+        await page.goto(url);
+        await page.waitForSelector("#cb1-edit");
+        await page.type("#cb1-edit", 'repetidor wi-fi');
+        await Promise.all([
+            page.waitForNavigation(),
+            page.click(".nav-search-btn")
+        ])
+
+        const links = await page.$$eval(".ui-search-result__image > a", el => el.map(link => link.href));
+
+        for (link of links) {
+            if (counter === 4) continue;
+            console.log('Mercado livre pagina', counter)
+            await page.goto(link);
+            await page.waitForSelector(".ui-pdp-title")
+
+            const title = await page.$eval(".ui-pdp-title", element => element.innerText);
+            const price = await page.$eval(".andes-money-amount__fraction", element => element.innerText);
+
+            const obj= {}
+
+            obj.title = title;
+            obj.price = price;
+            obj.link = link;
+
+            list.push(obj);
+
+            console.log(list)
+
+
+            counter++;
+        }
+
+        await browser.close()
+
+
+    })();
